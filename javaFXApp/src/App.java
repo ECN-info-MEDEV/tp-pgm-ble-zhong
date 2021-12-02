0462d6efaee944f728ef7f2c780264da378c9fa5
ZGChung <69182806+ZGChung@users.noreply.github.com>
2021-12-02 11:48:33 +0100

    feat: framework with 3 buttons

--- javaFXApp/src/App.java
+++ javaFXApp/src/App.java
@@ -3,31 +3,71 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
 
 public class App extends Application {
     public static void main(String[] args) {
 
-        // launch(args);
+        launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello World!");
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Say 'Hello World1'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                // System.out.println("Hello World!1");
+                Print p = new Print();
+                p.hello1();
             }
         });
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        Button btn2 = new Button();
+        btn2.setText("Say 'Hello World2'");
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                // System.out.println("Hello World!2");
+                Print p = new Print();
+                p.hello2();
+            }
+        });
+
+        Button btn3 = new Button();
+        btn3.setText("Open Dialog");
+        btn3.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                final Stage dialog = new Stage();
+                dialog.initModality(Modality.APPLICATION_MODAL);
+                dialog.initOwner(primaryStage);
+                VBox dialogVbox = new VBox(20);
+                dialogVbox.getChildren().add(new Text("This is a Dialog"));
+                Scene dialogScene = new Scene(dialogVbox, 300, 200);
+                dialog.setScene(dialogScene);
+                dialog.show();
+            }
+
+        });
+
+        HBox topMenu = new HBox();
+        topMenu.getChildren().add(btn);
+        HBox lowMenu = new HBox();
+        lowMenu.getChildren().add(btn2);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(topMenu);
+        borderPane.setCenter(lowMenu);
+
+        primaryStage.setScene(new Scene(borderPane, 300, 250));
         primaryStage.show();
     }
 }
---
+++ javaFXApp/src/Print.java
@@ -0,0 +1,9 @@
+public class Print {
+    public void hello1() {
+        System.out.println("Hello World!1");
+    }
+
+    public void hello2() {
+        System.out.println("Hello World!2");
+    }
+}
