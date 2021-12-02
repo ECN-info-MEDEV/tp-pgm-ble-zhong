c58400b40b1fc26c4bc415f8c508fe6ff98162aa
ZGChung <69182806+ZGChung@users.noreply.github.com>
2021-12-02 10:48:42 +0100

    feat: framework of histogram method

--- mavenproject1/src/main/java/edu/centralenantes/mavenproject1/HelloWorld.java
+++ mavenproject1/src/main/java/edu/centralenantes/mavenproject1/HelloWorld.java
@@ -16,7 +16,9 @@ public class HelloWorld {
      */
     // FIXME 1: this is a fix
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World\n");
+        Image i = new Image();
+        i.histo(1);
     }
     // TODO 1: This is an action item
     // TODO 2: this is the second action item
